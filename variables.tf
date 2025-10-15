a50c7f0f81d139008bfe28d8bdc60e940e83641e
Sebastián Olaya <solayap@unal.edu.co>
2025-10-14 20:30:20 -0500

    Move terraform and provider blocks to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "ssh_key" {
   description = "Provides custom public SSH key."
   type        = string
